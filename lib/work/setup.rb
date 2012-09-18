245b1328fcac052d44c8c338e53df6b2b647b66b
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-09-18 14:24:32 -0400

    update

--- lib/work/setup.rb
+++ lib/work/setup.rb
@@ -4,7 +4,7 @@ module Work
       puts "Setup Work..."
       new_options = {}
       Work::Configuration.config_options.each_pair do |name, value|
-        puts "#{name}  (#{value}):"
+        print "#{name}  (#{value}):  "
         input = gets
         if (input.strip == "")
           new_options[name] = value
@@ -12,6 +12,7 @@ module Work
           new_options[name] = input.strip
         end
       end
+      Work::Configuration.save_options(new_options)
       puts "Configuration Completed..."
     end
   end
