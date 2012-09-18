d6d5db397b903f6e3790bd7c9fc68eb01f0a3ac6
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-09-18 14:20:36 -0400

    update

--- lib/work.rb
+++ lib/work.rb
@@ -2,12 +2,11 @@ require "work/version"
 
 module Work
   def self.run(command, args)
-    switch command.downcase
-      when 'setup'
-        Work::Setup.execute(command, args)
-      else
-        puts "Huh?"
-      end
+    case command.downcase
+    when 'setup'
+      Work::Setup.execute(command, args)
+    else
+      puts "Huh?"
     end
   end
 end
--- lib/ziplist_workflow.rb
+++
@@ -1,5 +0,0 @@
-class ZiplistWorkflow
-  def self.run(command, args)
-    puts "Running..."
-  end
-end
