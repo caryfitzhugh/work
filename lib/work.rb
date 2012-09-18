81b2feb64d4c29309affae9d5dc7de1d525a1206
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-09-18 14:40:54 -0400

    update

--- lib/work.rb
+++ lib/work.rb
@@ -2,13 +2,17 @@ require "work/version"
 require "work/setup"
 require "work/configuration"
 require "work/create"
+require "work/current"
 require "work/git"
+require 'pp'
 
 module Work
   def self.run(command, args)
     case command.downcase
     when 'setup'
       Work::Setup.execute(args)
+    when 'current'
+      Work::Current.execute(args)
     when 'create'
       Work::Create.execute(args)
     else
